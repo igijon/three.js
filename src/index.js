07b816c0b19a5d61e326b0c2e9f80c90e15ca7cf
Inma Gijón <igijoninf@gmail.com>
2023-03-15 19:12:30 +0100

    Alternativa set camera

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import renderer from "./js/Renderer"
 import cube from "./js/Box"
 
 scene.add(cube);
-camera.position.z = 5;
-camera.position.x = 3;
+// camera.position.z = 5;
+// camera.position.x = 3;
+// camera.position.y = 1;
+camera.position.set(5,3,1);
 camera.lookAt(cube.position);
 renderer.render(scene, camera);
