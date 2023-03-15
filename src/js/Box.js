28ee3a30e70ac39aa6c4a13f321a10c142978c43
Inma Gijón <igijoninf@gmail.com>
2023-03-15 19:15:45 +0100

    Materiales

--- src/js/Box.js
+++ src/js/Box.js
@@ -1,7 +1,8 @@
 import * as THREE from 'three';
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } ); //No se ve afectado por las luces
+const material = new THREE.MeshNormalMaterial( { color: 0x00ff00 } ); //Pinta las caras en función de las normales
 const cube = new THREE.Mesh( geometry, material );
 
 export default cube
