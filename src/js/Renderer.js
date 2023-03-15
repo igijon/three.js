0f6dc51966a6e3e5828366c4b15eee55abf80b68
Inma Gijón <igijoninf@gmail.com>
2023-03-15 17:20:49 +0100

    Renderer en el html y consola

--- src/js/Renderer.js
+++ src/js/Renderer.js
@@ -4,4 +4,6 @@ const renderer = new THREE.WebGLRenderer({antialias:true});
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+console.log(renderer.domElement);
+
 export default renderer
