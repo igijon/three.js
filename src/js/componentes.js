a0179600fcfa094d80040119bcc7c65880c74e87
Inma Gijón <igijoninf@gmail.com>
2023-03-15 13:21:34 +0100

    Webpack initial project

---
+++ src/js/componentes.js
@@ -0,0 +1,7 @@
+
+import '../css/components.css'
+// import webpacklogo from '../assets/img/webpack-logo.png'
+
+export const saludar = ( nombre ) => {
+    console.log('Creando etiqueta h1');
+} 
---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+const path = require('path');
+const filesHTML = [
+    {
+        filename: 'index.html',
+        chunks: ['index']
+    }
+]
+
+module.exports = {
+    mode: 'development',
+    devServer: {
+        historyApiFallback: true,
+        allowedHosts: 'all',
+        static: {
+            directory: path.join(__dirname, '/')
+        },
+        hot: true,
+        open:true
+    },
+    output: {
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+                options: {
+                    sources: false
+                }
+            },       
+            {
+                test: /\.(c|sc|sa)ss$/,
+                use: [ MiniCssExtractPlugin.loader, 
+                    'css-loader',
+                    'sass-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: () => {
+                                    require('autoprefixer')
+                                }
+                            }
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                loader: 'file-loader'
+            }
+        ]
+    },
+    entry: {
+        index: './src/index.js'
+    },
+    optimization: {},
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            ignoreOrder: false
+        }),
+        new CopyPlugin({
+            patterns: [
+                {from: 'src/assets/', to: 'assets/'}
+            ]
+        })
+    ].concat(filesHTML.map((templateFile) => new HtmlWebPackPlugin({
+        filename: templateFile.filename,
+        template: './src/'+templateFile.filename,
+        chunks: templateFile.chunks,
+        inject: (templateFile.chunks.length==0) ? false: true
+    })))
+};
