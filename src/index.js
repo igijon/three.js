454a1071ff77e92f3755b704f513676a17d892d4
Inma Gijón <igijoninf@gmail.com>
2023-03-16 13:08:58 +0100

    KeyListener

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import cube from "./js/Box"
 import light from './js/Light'
 import resize from './js/Resize'
 import loopMachine from './js/LoopMachine'
+import keyListener from './js/KeyListener'
 
 scene.add(cube);
 // camera.position.z = 5;
@@ -15,9 +16,13 @@ camera.position.set(5,3,1);
 camera.lookAt(cube.position);
 scene.add(light);
 resize.start(renderer);
-
 loopMachine.addCallback(() => {
     cube.rotation.y+=0.01
     renderer.render(scene, camera);
 }); 
 loopMachine.start();
+keyListener.start();
+
+setTimeout(() => {
+    console.log(keyListener.isPressed(13));
+}, 3000);
