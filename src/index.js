5551ffe14bf0e23905ff43dd8ff9c1b9ce84b7b1
Inma Gijón <igijoninf@gmail.com>
2023-03-16 12:15:56 +0100

    Resize

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import camera from "./js/Camera"
 import renderer from "./js/Renderer"
 import cube from "./js/Box"
 import light from './js/Light'
+import resize from './js/Resize'
 
 scene.add(cube);
 // camera.position.z = 5;
@@ -12,6 +13,7 @@ scene.add(cube);
 camera.position.set(5,3,1);
 camera.lookAt(cube.position);
 scene.add(light);
+resize.start(renderer);
 
 setInterval(() => {
     cube.rotation.y+=0.01
